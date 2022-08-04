ff86a263c29eeac2471f7a41489f6ea319c390fc
ankit723 <72332524+ankit723@users.noreply.github.com>
2022-08-04 18:05:15 +0530

    Updates

---
+++ precache-manifest.8e54be48e1d84aa1ac8029ccfeb6049c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ab9dd622b0945188efe",
+    "url": "/mukularanayam-school-website/static/css/main.b8bcadbf.chunk.css"
+  },
+  {
+    "revision": "6ab9dd622b0945188efe",
+    "url": "/mukularanayam-school-website/static/js/main.6ab9dd62.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/mukularanayam-school-website/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "4b65be73075854d4b6b3",
+    "url": "/mukularanayam-school-website/static/js/2.4b65be73.chunk.js"
+  },
+  {
+    "revision": "74d65cfbc0d08aa43c97",
+    "url": "/mukularanayam-school-website/static/js/runtime~main.74d65cfb.js"
+  },
+  {
+    "revision": "b7bc84121f608d606e479a4399c63ca4",
+    "url": "/mukularanayam-school-website/static/media/about-bg.b7bc8412.jpg"
+  },
+  {
+    "revision": "a5308c1b54713a8e55c16969f2daa866",
+    "url": "/mukularanayam-school-website/static/media/cbse.a5308c1b.png"
+  },
+  {
+    "revision": "5018c5566f04358db48e8e6bd3ddfce3",
+    "url": "/mukularanayam-school-website/static/media/fee.5018c556.png"
+  },
+  {
+    "revision": "28d38abb53673a89e3d00816a11b8e32",
+    "url": "/mukularanayam-school-website/static/media/lb.28d38abb.png"
+  },
+  {
+    "revision": "71846599a4f19e77d01817b31aae3eb0",
+    "url": "/mukularanayam-school-website/static/media/instagram.71846599.png"
+  },
+  {
+    "revision": "58690d26328eb4b095589ce93766c4f8",
+    "url": "/mukularanayam-school-website/static/media/twitter.58690d26.png"
+  },
+  {
+    "revision": "98e4733a5ece78fe444a09b6b5e82ad2",
+    "url": "/mukularanayam-school-website/static/media/whatsapp.98e4733a.png"
+  },
+  {
+    "revision": "27fa292c65b0123bb284fcd71ffc303b",
+    "url": "/mukularanayam-school-website/static/media/bg2.27fa292c.jpg"
+  },
+  {
+    "revision": "b6031dffad001c2be8abda9f8f3f0744",
+    "url": "/mukularanayam-school-website/static/media/bg3.b6031dff.jpg"
+  },
+  {
+    "revision": "47cf3b8ff225d51a4046862b40429fe4",
+    "url": "/mukularanayam-school-website/static/media/bg4.47cf3b8f.jpg"
+  },
+  {
+    "revision": "05fcb141bf5400c119d68f4fab1c2384",
+    "url": "/mukularanayam-school-website/static/media/addmisions.05fcb141.jpg"
+  },
+  {
+    "revision": "354a0d3d1aeb40e14e34451206bfe11b",
+    "url": "/mukularanayam-school-website/index.html"
+  }
+];
