d9e9af571d7b642125e73f4e783ff28713302078
ankit723 <72332524+ankit723@users.noreply.github.com>
2023-04-02 13:06:04 +0530

    Things changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ export function Navbar(){
             </div>
 
             <div class="right1">
-                <Link to="addmissions.html">Enroll Now</Link>
+                <Link to="ddmissions">Enroll Now</Link>
             </div>
         </header>
     )
